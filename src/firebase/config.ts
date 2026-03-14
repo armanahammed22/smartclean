3310d8c4929843481efd6509e0c692ed04907e77
Smart Clean <smartclean422@gmail.com>
2026-03-14 16:36:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5425564660-b1f7f",
+  "appId": "1:1012639715946:web:c5bda72aff647d14d1b013",
+  "apiKey": "AIzaSyA2LPcx8rgQNwzgerxh1PoXynBkvOcrHE0",
+  "authDomain": "studio-5425564660-b1f7f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1012639715946"
+};
